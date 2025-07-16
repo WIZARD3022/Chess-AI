3a1c3a0bb0d3684a80eff4464f3d3251459221f8
WIZARD3022 <vinitsheetal15@gmail.com>
2025-07-16 20:37:12 +0530

    Auto commit

--- chess.py
+++ chess.py
@@ -1,14 +1,48 @@
 import pygame
 
 class Board:
-    def __init__(self, width, height):
+    def __init__(self, width, height, screen):
         self.width = width
         self.height = height
         self.board = [[None for _ in range(8)] for _ in range(8)]
-        self.init_board()
+        self.init_board(screen)
 
-    def init_board(self):
-        
+    def load_image(path):
+        return pygame.transform.scale(path, (100, 100))
+
+    def init_board(self, screen):
+        BE = pygame.image.load("./image/Black_elephant.png")
+        BE = self.load_image(BE)
+        WE = pygame.image.load("./image/White_elephant.png")
+        WE = self.load_image(WE)
+
+        BH = pygame.image.load("./image/Black_horse.png")
+        BH = self.load_image(BH)
+        WH = pygame.image.load("./image/White_horse.png")
+        WH = self.load_image(WH)
+
+        BK = pygame.image.load("./image/Black_King.png")
+        BK = self.load_image(BK)
+        WK = pygame.image.load("./image/White_King.png")
+        WK = self.load_image(WK)
+
+        BM = pygame.image.load("./image/Black_minister.png")
+        BM = self.load_image(BM)
+        WM = pygame.image.load("./image/White_minister.png")
+        WM = self.load_image(WM)
+
+        BP = pygame.image.load("./image/Black_pawn.png")
+        BP = self.load_image(BP)
+        WP = pygame.image.load("./image/White_pawn.png")
+        WP = self.load_image(WP)
+
+        BQ = pygame.image.load("./image/Black_queen.png")
+        BQ = self.load_image(BQ)
+        WQ = pygame.image.load("./image/White_queen.png")
+        WQ = self.load_image(WQ)
+
+        screen.blit(BE, (100, 100))
+
 
         pass
 
--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import pygame
 import sys
 from chess import Board
 
-def load_image(path):
-    return pygame.transform.scale(path, (100, 100))
-
 
 def main():
 
@@ -20,50 +17,19 @@ def main():
     clock = pygame.time.Clock()
     FPS = 60
 
-    board = Board(WIDTH, HEIGHT)
-
-    BE = pygame.image.load("./image/Black_elephant.png")
-    BE = load_image(BE)
-    WE = pygame.image.load("./image/White_elephant.png")
-    WE = load_image(WE)
-
-    BH = pygame.image.load("./image/Black_horse.png")
-    BH = load_image(BH)
-    WH = pygame.image.load("./image/White_horse.png")
-    WH = load_image(WH)
-
-    BK = pygame.image.load("./image/Black_King.png")
-    BK = load_image(BK)
-    WK = pygame.image.load("./image/White_King.png")
-    WK = load_image(WK)
-
-    BM = pygame.image.load("./image/Black_minister.png")
-    BM = load_image(BM)
-    WM = pygame.image.load("./image/White_minister.png")
-    WM = load_image(WM)
-
-    BP = pygame.image.load("./image/Black_pawn.png")
-    BP = load_image(BP)
-    WP = pygame.image.load("./image/White_pawn.png")
-    WP = load_image(WP)
-
-    BQ = pygame.image.load("./image/Black_queen.png")
-    BQ = load_image(BQ)
-    WQ = pygame.image.load("./image/White_queen.png")
-    WQ = load_image(WQ)
-
+    board = Board(WIDTH, HEIGHT, screen)
 
     # Game Loop
     running = True
     while running:
         # clock.tick(FPS)  # Limit FPS
         pygame.display.set_caption(f"Chess fps :{clock.tick(FPS)}")
-        screen.blit(BE, (100, 100))
-        screen.blit(WE, (200, 100))
-        screen.blit(BQ, (300, 100))
-        screen.blit(WQ, (400, 100))
-        screen.blit(BK, (200, 300))
-        screen.blit(WK, (200, 300))
+        # screen.blit(BE, (100, 100))
+        # screen.blit(WE, (200, 100))
+        # screen.blit(BQ, (300, 100))
+        # screen.blit(WQ, (400, 100))
+        # screen.blit(BK, (200, 300))
+        # screen.blit(WK, (200, 300))
         # screen.blit(BE, (100, 100))
 
         # screen.fill(BLACK)  # Clear screen
