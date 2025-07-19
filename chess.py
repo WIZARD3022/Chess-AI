ae424dc254e89337281db9a229be043028868a9a
WIZARD3022 <vinitsheetal15@gmail.com>
2025-07-19 17:25:10 +0530

    Auto commit

--- chess.py
+++ chess.py
@@ -98,6 +98,7 @@ class Board:
         # self.board[start_row][start_col] = 0
         # self.board[end_row][end_col] = piece
         self.board[end_row][end_col], self.board[start_row][start_col] = self.board[start_row][start_col], self.board[end_row][end_col]
+        print(f"Moved piece from ({start_row}, {start_col}) to ({end_row}, {end_col})")
         for i in range(8):
             print(self.board[i])
 
